fa242c2bf0ce90a282a5667a424c879b62ca1593
Bulemu Shilukukwa <narcoa47@gmail.com>
2024-02-24 12:20:36 -0800

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)*u=nwt$jyzekd!a*awe=a93qh76(@ddnci&f_=q#_u^5yjtp_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

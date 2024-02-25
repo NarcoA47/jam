379d45a86a8ba0c524374c1932fff94026b8098d
Bulemu Shilukukwa <narcoa47@gmail.com>
2024-02-25 00:50:07 -0800

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*u=nwt$jyzekd!a*awe=a93qh76(@ddnci&f_=q#_u^5yjtp_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
